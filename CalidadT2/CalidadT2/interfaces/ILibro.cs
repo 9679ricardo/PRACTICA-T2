0e04967badb859dfd5c290691b62de3b10799af2
9679ricardo <66107803+9679ricardo@users.noreply.github.com>
2022-10-20 17:27:23 -0500

    ILibros

---
+++ CalidadT2/CalidadT2/interfaces/ILibro.cs
@@ -0,0 +1,13 @@
+﻿using CalidadT2.Models;
+using System.Collections.Generic;
+
+namespace CalidadT2.interfaces
+{
+    public interface ILibro
+    {
+        List<ILibro> getListLibro(int id);
+        ILibro getLibro(int id);
+        void addLibro(int id);
+        void addComentario(Comentario comentario, int id);
+    }
+}
